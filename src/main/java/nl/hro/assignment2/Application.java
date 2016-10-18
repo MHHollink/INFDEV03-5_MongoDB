4c058618a5aab102f85c0bbb17b83ce368c8082f
mjollnir94 <marcel_Hollink@hotmail.com>
2016-10-18 13:40:48 +0200

    application discriptive comment updated

--- src/main/java/nl/hro/assignment2/Application.java
+++ src/main/java/nl/hro/assignment2/Application.java
@@ -1,7 +1,14 @@
 package nl.hro.assignment2;
 
 /**
- * Created by marcel on 18-10-2016.
+ * Java application for loading in a bunch of models in a MongoDB Document database.
+ *
+ * @author Marcel Hollink
+ * @version 0.0.1
+ * @since 18-10-2016
  */
 public class Application {
+
+
+    
 }
